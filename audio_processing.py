e5c97a1065dcc8c220f329b9473e276d5cdacf64
Paulo Henrique Pianissola de Cerqueira <paulophpc@gmail.com>
2024-10-12 20:48:36 +0100

    Updated audio_processing.py to include ML

--- audio_processing.py
+++ audio_processing.py
@@ -2,9 +2,15 @@ import numpy as np
 import wave
 import io
 import json
-#from emotion_predictor import analyze_audio  # Importe a função da sua rede neural
+from transformers import Wav2Vec2Processor, HubertForSequenceClassification, AutoFeatureExtractor
+import torch
 
 SAMPLE_RATE = 16000
+MODEL_NAME = "superb/hubert-base-superb-er"
+
+feature_extractor = AutoFeatureExtractor.from_pretrained(MODEL_NAME)
+model = HubertForSequenceClassification.from_pretrained(MODEL_NAME)
+
 
 def pcm_to_wav(pcm_data: bytes) -> bytes:
     """Converte PCM para formato WAV."""
@@ -17,17 +23,27 @@ def pcm_to_wav(pcm_data: bytes) -> bytes:
     wav_buffer.seek(0)
     return wav_buffer.read()
 
-def process_audio_chunk(pcm_data: bytes, timestamp: float) -> str:
+def process_audio_chunk(pcm_data: np.ndarray, timestamp: float) -> str:
     """Processa um chunk de áudio e analisa com a rede neural."""
-    wav_data = pcm_to_wav(pcm_data)
-    
-    # Chame a função de análise da rede neural
-    #analysis_result = analyze_audio(wav_data)
+    #wav_data = pcm_to_wav(pcm_data)
 
+    #Analise rede neuronal: Input array
+    inputs = feature_extractor(pcm_data, sampling_rate=SAMPLE_RATE, return_tensors="pt", padding = True)
+    with torch.no_grad():
+        logits = model(**inputs).logits
+    
+    predict_id = torch.argmax(logits, dim=-1).item()
+    emotion = model.config.id2label[predict_id]
+    confidence = torch.softmax(logits, dim=-1)[0][predict_id].item()
+    
+    #Converte a confiança para percentagem
+    confidence_percent = round(confidence * 100, 2)
+    
     # Retorne o resultado em formato JSON com o timestamp
     response = {
-    "timestamp": timestamp,
-        "analysis": "analysis_result"
+        "timestamp": timestamp,
+        "emotion": emotion,
+        "confidence": confidence_percent
     }
     
     return json.dumps(response)
